2aa2e071e3bc071d6138ec15ab5988745926a85e
Asdarsaid377 <106737412+Asdarsaid377@users.noreply.github.com>
2022-08-23 08:58:01 +0800

    k

--- components/card/Card.jsx
+++ components/card/Card.jsx
@@ -15,7 +15,7 @@ const Card = (props) => {
 					/>
 				</div>
 				<Link href="/list_produk">
-					<button className="md:px-8 hover:scale-y-110 duration-300 z-30 absolute delay-200 py-2 bg-gray-100 bg-opacity-40 font-semibold text-black rounded-lg mt-[-80px] ml-3">
+					<button className="px-8 hover:scale-y-110 duration-300 z-30 absolute delay-200 py-2 bg-gray-100 bg-opacity-40 font-semibold text-black rounded-lg mt-[-80px] ml-3">
 						{props.title}
 					</button>
 				</Link>
